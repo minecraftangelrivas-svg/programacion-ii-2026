c19b873cd442e0f894e321763dbaa04c64f286e1
Angel Rivas <minecraftangelrivas@gmail.com>
2026-08-23 22:34:21 -0600

    Update Main.java

--- Tarea 4 - POO - Sistema de gestión de envíos/src/Main.java
+++ Tarea 4 - POO - Sistema de gestión de envíos/src/Main.java
@@ -1,2 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
-}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int opcion = 0;
+
+        do {
+            System.out.println("\nSISTEMA DE ENVÍOS");
+            System.out.println("1. Registrar envío nacional");
+            System.out.println("2. Registrar envío internacional");
+            System.out.println("3. Salir");
+     
+
+            opcion = scanner.nextInt();
+
+            if (opcion == 1 || opcion == 2) {
+                // Valida que no esté vacío
+                String codigo = "";
+                while (codigo.trim().isEmpty()) {// trim quita espacios extras y isEmpty revisa si quedó vacío
+                    System.out.print("Ingrese el codigo del envío: ");
+                    codigo = scanner.nextLine();
+                    if (codigo.trim().isEmpty()) {
+                        System.out.println("Error: El codigo no puede estar vacío.");
+                    }
+                }
+            }
+        }
+    }
